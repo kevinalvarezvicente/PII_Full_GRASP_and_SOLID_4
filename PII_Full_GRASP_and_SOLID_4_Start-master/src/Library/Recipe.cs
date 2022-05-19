2993b347bca4d1c4991daa5731e763789f8c549c
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2022-05-19 19:13:44 -0300

    Add files via upload

---
+++ PII_Full_GRASP_and_SOLID_4_Start-master/src/Library/Recipe.cs
@@ -0,0 +1,59 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Full_GRASP_And_SOLID
+{
+    public class Recipe
+    {
+        private IList<Step> steps = new List<Step>();
+
+        public Product FinalProduct { get; set; }
+        //Segun creator es Recipe quien deberia de crear objetos de tipo Step
+        public Step AddStep(Product input, double quantity, Equipment equipment, int time)
+        {
+            Step step = new Step( input, quantity, equipment, time );
+            this.steps.Add(step);
+            return step;
+        }
+
+        public void RemoveStep(Step step)
+        {
+            this.steps.Remove(step);
+        }
+
+        // Agregado por SRP
+        public string GetTextToPrint()
+        {
+            string result = $"Receta de {this.FinalProduct.Description}:\n";
+            foreach (Step step in this.steps)
+            {
+                result = result + step.GetTextToPrint() + "\n";
+            }
+
+            // Agregado por Expert
+            result = result + $"Costo de producción: {this.GetProductionCost()}";
+
+            return result;
+        }
+
+        // Agregado por Expert
+        public double GetProductionCost()
+        {
+            double result = 0;
+
+            foreach (Step step in this.steps)
+            {
+                result = result + step.GetStepCost();
+            }
+
+            return result;
+        }
+
+    }
+}
